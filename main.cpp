968a3e8586922342c92d63f4a2fb68070341ad3f
tah9 <690818882@qq.com>
2023-03-09 22:39:09 +0800

    单词翻转

--- main.cpp
+++ main.cpp
@@ -77,34 +77,107 @@ ListNode *Merge(ListNode *pHead1, ListNode *pHead2) {
             last->next = pHead2;
             pHead2 = pHead2->next;
         }
-        last=last->next;
+        last = last->next;
     }
     while (pHead1) {
         last->next = pHead1;
         pHead1 = pHead1->next;
-        last=last->next;
+        last = last->next;
 
     }
     while (pHead2) {
         last->next = pHead2;
         pHead2 = pHead2->next;
-        last=last->next;
+        last = last->next;
 
     }
     return head->next;
 }
 
+class Solution {
+public:
+    int arr[300];
+    int topIndex = 0;
+    int minIndex = 0;
+
+    void push(int value) {
+        arr[topIndex++] = value;
+    }
+
+    void pop() {
+        topIndex--;
+    }
+
+    int top() {
+        return arr[topIndex - 1];
+    }
+
+    int min() {
+        int minNum = arr[0];
+        for (int i = 0; i < topIndex; ++i) {
+            if (arr[i] <= minNum) {
+                minNum = arr[i];
+                minIndex = i;
+            }
+        }
+        return minNum;
+    }
+};
+
+string ReverseSentence(string str) {
+    vector<string> vStr;
+    int startIndex = 0;
+    int len = 0;
+    for (int i = 0; i < str.length(); ++i) {
+        if (str[i] == ' ') {
+//            if(len==0){
+//                len=1;
+//            }
+            vStr.push_back(str.substr(startIndex, len));
+            startIndex = i + 1;
+            len = 0;
+        } else {
+            len++;
+        }
+    }
+    vStr.push_back(str.substr(startIndex, str.length() + 1 - startIndex));
+
+    string resultStr;
+    while (!vStr.empty()) {
+        resultStr += (vStr.back() + " ");
+        vStr.pop_back();
+    }
+    return resultStr.substr(0, resultStr.length()-1);
+}
+
 int main(void) {
-    int arr1[] = {1, 3, 5};
-    int arr2[] = {2, 4, 6};
-//    int arr[] = {1, 1};
-    ListNode *head = createListNode(arr1, len(arr1));
-    ListNode *head2 = createListNode(arr2, len(arr2));
+    string originStr = "nowcoder. a am I dsfsd";
+    string str = ReverseSentence(originStr);
+    printf("%s", str.c_str());
+//    printf("%s", originStr.substr(10,1).c_str());
+//    vector<int> v1{1,2,3,4,5};
+//    vector<int> v2{4,5,3,2,1};
+//    for(auto item:v1){
+//        if (item==2){
+//            v1.pop_back();
+//        }
+//    }
+//    for(auto item:v1){
+//        printf("%d\n", item);
+//    }
 
-    ListNode *m = Merge(head, head2);
 
 
-    printListNode(m);
+//    int arr1[] = {1, 3, 5};
+//    int arr2[] = {2, 4, 6};
+////    int arr[] = {1, 1};
+//    ListNode *head = createListNode(arr1, len(arr1));
+//    ListNode *head2 = createListNode(arr2, len(arr2));
+//
+//    ListNode *m = Merge(head, head2);
+//
+//
+//    printListNode(m);
 //    ListNode *h = deleteDuplication(head);
 //    printListNode(h);
 
